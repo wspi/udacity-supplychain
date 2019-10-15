50e23492a99a2db9e80543b489a1615b893aba68
wspi <wspi>
2019-10-15 19:48:35 +0000

    pushing project

---
+++ README.md
@@ -0,0 +1,23 @@
+## Supplychain Project
+
+### Blockchain info
+
+ - Contract Address: 0x1e7E38A91F55A25758ef53f9cF75214B1e9974Ed
+
+
+### libraries
+ - truffle-hdwallet-provider: used to deploy contract using infura
+ - truffle-assertions: used to run tests on truffle
+
+
+### Program versions
+ - Truffle v5.0.37 (core: 5.0.37)
+ - Solidity v0.5.8 (solc-js)
+ - Node v10.16.3
+ - Web3.js v1.2.1
+
+### UML Documents
+ - [Activity](uml/activity.png)
+ - [Sequence](uml/sequence.png)
+ - [State](uml/state.png)
+ - [Class](uml/class.png)
---
+++ project-6/truffle.js
@@ -0,0 +1,21 @@
+const HDWallet = require('truffle-hdwallet-provider');
+const infuraKey = "";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWallet(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
+    }
+  }
+};
